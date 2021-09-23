24545e82273993b318a022a907a72fd6512f4f5c
Maurizio Tonelli <mauriziotonelli@hotmail.com>
2021-09-22 22:25:21 -0500

    Add first version of the contact page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
 import { ClearNavbarComponent } from './shared/clear-navbar/clear-navbar.component';
 import { WelcomeCardComponent } from './landing-page/welcome-card/welcome-card.component';
 import { ExampleSectionComponent } from './landing-page/example-section/example-section.component';
 import { LandingComponent } from './landing-page/landing/landing.component';
 import { PlanCardComponent } from './premium-page/plan-card/plan-card.component';
 import { PremiumComponent } from './premium-page/premium/premium.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { ContactComponent } from './contact-page/contact/contact.component';
+import { ContactFormComponent } from './contact-page/contact-form/contact-form.component';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
@@ -21,12 +27,17 @@ import { PremiumComponent } from './premium-page/premium/premium.component';
     LandingComponent,
     PlanCardComponent,
     PremiumComponent,
+    ContactPageComponent,
+    ContactComponent,
+    ContactFormComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatInputModule,
+    MatCheckboxModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
